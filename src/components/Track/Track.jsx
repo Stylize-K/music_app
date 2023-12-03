7c3df6a19326b864b492e6908fb8a16e0bdae0d3
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-12-03 19:39:44 +0200

    add search input and format duration

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,9 +1,11 @@
 import style from "./Track.module.scss";
 import { IconButton } from "@mui/material";
 import { PlayArrow } from "@mui/icons-material";
+import secondsToMMSS from "../../utils/secondsToMMSS";
 
 const Track = (track) => {
   const { id, preview, title, artists, duration } = track;
+  const formattedDuration = secondsToMMSS(duration);
 
   return (
     <div className={style.track}>
@@ -15,7 +17,7 @@ const Track = (track) => {
         <b>{title}</b>
         <p>{artists}</p>
       </div>
-      <p>{duration}</p>
+      <p>{formattedDuration}</p>
     </div>
   );
 };
